6450151e4ea6b0bcc4028ec9d4c69fd2772a73db
dominthebox <dsallustro@gmail.com>
2021-08-10 20:45:09 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567891234',
+        make: 'subaru',
+        model: 'wrx',
+        mileage: 59876,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '2345678912345',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 69890,
+        title: 'dirty',
+        transmission: 'automatic',
+    },
+    {
+        vin: '3456789123456',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
